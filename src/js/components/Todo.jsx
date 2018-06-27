b0db96fd4e9d02006e2fea228a9a44005412b415
Stanislaw Gregor <gregor.stanislaw@hotmail.com>
2018-06-27 22:52:59 +0200

    Working removeTodo handler

--- src/js/components/Todo.jsx
+++ src/js/components/Todo.jsx
@@ -7,13 +7,13 @@ class Todo extends Component {
       <div className="card card-body">
         <div className="row">
           <div className="col-1">
-            <input type="checkbox" />
+            <input type="checkbox" onClick={this.props.toggleTodo} />
           </div>
           <div className="col-10">
             <h4>{this.props.text}</h4>
           </div>
           <div className="col-1">
-            <FontAwesome name="times" />
+            <FontAwesome name="times" onClick={this.props.removeTodo} />
           </div>
         </div>
       </div>
